4e84b8028313c6847d4f83d957963dfd784d8bbc
Jørgen Nordmoen <jorgen@nordmoen.net>
2024-09-13 15:09:21 +0200

    La til CLI

---
+++ src/nks_kbs_analyse/cli/settings.py
@@ -0,0 +1,24 @@
+"""Innstillinger fra miljøvariabler."""
+
+from pydantic import Field, HttpUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """Konfigurasjon for CLI."""
+
+    model_config = SettingsConfigDict(
+        env_prefix="nks_bob_",
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_nested_delimiter="__",
+    )
+
+    vdb_url: HttpUrl = Field("https://nks-vdb.ansatt.dev.nav.no")
+    """URL til NKS-VDB tjenesten"""
+
+    kbs_url: HttpUrl = Field("https://nks-kbs.ansatt.dev.nav.no")
+    """URL til NKS-KBS tjenesten"""
+
+
+settings = Settings()  # type: ignore[call-arg]
