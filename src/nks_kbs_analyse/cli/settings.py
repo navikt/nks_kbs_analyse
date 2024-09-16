8533b1808c6cfe6ac9126f8399d1d44d21a13904
Jørgen Nordmoen <jorgen@nordmoen.net>
2024-09-16 13:33:17 +0200

    Ignorer miljøvariabler vi ikke bryr oss om

--- src/nks_kbs_analyse/cli/settings.py
+++ src/nks_kbs_analyse/cli/settings.py
@@ -12,6 +12,7 @@ class Settings(BaseSettings):
         env_file=".env",
         env_file_encoding="utf-8",
         env_nested_delimiter="__",
+        extra="ignore",
     )
 
     vdb_url: HttpUrl = Field("https://nks-vdb.ansatt.dev.nav.no")
